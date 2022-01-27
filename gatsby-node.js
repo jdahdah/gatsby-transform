92f2179ee02b722ec2a842a71cd2163390efca77
Jerome Dahdah <developer@dahdah.org>
2022-01-27 10:40:27 +0200

    * cleanup

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,8 @@
 exports.onCreateNode = async ({ node, loadNodeContent }) => {
   if (node.name !== "example") return;
 
+  // Ensure Gatsby loads the item into memory so that its
+  // content becomes available in the GraphQL File node
   try {
     const nodeContent = await loadNodeContent(node);
     console.log(node);
